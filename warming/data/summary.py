ee9cb18ee20634bb4278d30207e7b2b45f97a4ce
Carla <carla.richter@univie.ac.at>
2023-03-24 15:09:27 +0100

    added issue 1, 2, 3

--- warming/data/summary.py
+++ warming/data/summary.py
@@ -41,10 +41,106 @@ class Summary:
         """
         return self._data["CH4C"]
 
+    def get_ch4c_data(self, capital: str):
+        """
+        returns ch4c data for given capital
+        :param capital: name of capital as string
+        :return : data for methane per year for the given capita
+        """
+        data = self.ch4c()
+        return data[capital]
+
+    def n2oc(self):
+        """
+        returns data of 'CH4C' for all countries
+        :return: `CH4C` data per capity per country.
+        :rtype: pandas.core.series.Series
+        """
+        return self._data["N2OC"]
+
+    def get_n2oc_data(self, capital: str):
+        """
+        returns N2OC data for given capital
+        :param capital: name of capital as string
+        :return : data for N2CO per year for the given capita
+        """
+        data = self.n2oc()
+        return data[capital]
+
+    def co2y(self):
+        """
+        returns data of 'CH4C' for all countries
+        :return: `CH4C` data per capity per country.
+        :rtype: pandas.core.series.Series
+        """
+        return self._data["CO2Y"]
+
+    def get_co2y_data(self, capital: str):
+        """
+        returns CO2Y data for given capital
+        :param capital: name of capital as string
+        :return : data for CO2Y per year for the given capita
+        """
+        data = self.co2y()
+        return data[capital]
+
+    def ch4y(self):
+        """
+        returns data of 'CH4Y' for all countries
+        :return: `CH4Y` data per capity per country.
+        :rtype: pandas.core.series.Series
+        """
+        return self._data["CH4Y"]
+
+    def get_ch4y_data(self, capital: str):
+        """
+        returns CH4Y data for given capital
+        :param capital: name of capital as string
+        :return : data for CH4Y per year for the given capita
+        """
+        data = self.ch4y()
+        return data[capital]
+
+    def n2oy(self):
+        """
+        returns data of 'N20Y' for all countries
+        :return: `N20Y` data per capity per country.
+        :rtype: pandas.core.series.Series
+        """
+        return self._data["N20Y"]
+
+    def get_n2oy_data(self, capital: str):
+        """
+        returns N20Y data for given capital
+        :param capital: name of capital as string
+        :return : data for N20Y per year for the given capita
+        """
+        data = self.n2oy()
+        return data[capital]
+
+    def get_any_data(self, gas: str, capital: str):
+        """
+        returns data of given gas for given caapita
+        :param gas: string -
+        :param capital: string name of capital
+        :return : data of given capital for given gas
+        """
+        data = self._data[gas]
+        return data[capital]
 
 
+
+
+
+
+
+
+
+
+
+
+#CH4Y and N20Y
 
 if __name__ == "__main__":
     sume = Summary()
-    print(sume._data)
-
+    print(sume.get_any_data("N2OY", "Zambia"))
